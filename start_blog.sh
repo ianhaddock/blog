33a9b7ce170eee074614d9fcf0b9533031529c9f
Ian Haddock <ihaddock@mac.com>
2023-03-01 11:10:12 -0800

    update start_blog.sh

--- start_blog.sh
+++ start_blog.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-if [ ! -s '/usr/local/var/app-instance/blogapp.sqlite' ]; then
+if [ ! -s '/blog/instance/blogapp.sqlite' ]; then
     flask --app app init-db
 fi
 
 waitress-serve --port=5000 --threads=1 --call app:create_app
-
