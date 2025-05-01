dc74c14bd775d9fac27303654d918c0bc64bc781
ian <ianh@mac.com>
2025-05-01 12:15:41 -0700

    pylint auth.py

--- app/auth.py
+++ app/auth.py
@@ -1,3 +1,5 @@
+""" blog auth.py """
+
 import functools
 from flask import (Blueprint, flash, g, redirect, render_template, request,
                    session, url_for, current_app as app)
@@ -10,6 +12,7 @@ bp = Blueprint('auth', __name__, url_prefix='/auth')
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
+    """ user registration """
 
     if not app.config['register']:
         return redirect(url_for("blog.index"))
@@ -52,6 +55,7 @@ def register():
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
+    """ user login """
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
@@ -78,6 +82,7 @@ def login():
 
 @bp.before_app_request
 def load_logged_in_user():
+    """ load logged in user """
     user_id = session.get('user_id')
 
     if user_id is None:
@@ -90,12 +95,14 @@ def load_logged_in_user():
 
 @bp.route('/logout')
 def logout():
+    """ logout current user """
     session.clear()
 
     return redirect(url_for('index'))
 
 
 def login_required(view):
+    """ redirect to login for unknown user """
     @functools.wraps(view)
     def wrapped_view(**kwargs):
         if g.user is None:
