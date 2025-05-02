dc192956d2d66d9f49e284964dd46db8419284b4
ian <ianh@mac.com>
2025-05-01 19:04:01 -0700

    pylint: tests/test_factory.py

--- tests/test_factory.py
+++ tests/test_factory.py
@@ -1,5 +1,9 @@
+""" test_factor.py """
+
 from app import create_app
 
+
 def test_config():
+    """ test config """
     assert not create_app().testing
     assert create_app({'TESTING': True}).testing
